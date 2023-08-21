87f5144bd9e418fb94cd9218971ec5e488192a96
ZanborPanel <141851980+ZanborPanel@users.noreply.github.com>
2023-08-21 21:20:42 +0330

    Update zanbor.sh

--- zanbor.sh
+++ zanbor.sh
@@ -194,12 +194,6 @@ echo "{\"development\":\"@ZanborPanel\",\"install_location\":\"server\",\"main_d
 source_file="/var/www/html/ZanborPanel/config.php"
 destination_file="/var/www/html/ZanborPanel/config.php.tmp"
 
-token="${1}"
-admin_id="${2}"
-db_name="${3}"
-db_user="${4}"
-db_pass="${5}"
-
 replace=$(cat "$source_file" | sed -e "s/\[\*TOKEN\*\]/${TOKEN}/g" -e "s/\[\*DEV\*\]/${CHAT_ID}/g" -e "s/\[\*DB-NAME\*\]/${dbname}/g" -e "s/\[\*DB-USER\*\]/${dbuser}/g" -e "s/\[\*DB-PASS\*\]/${dbpass}/g")
 echo "$replace" > "$destination_file"
 mv "$destination_file" "$source_file"
