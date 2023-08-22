d67e62aad5515e1559977d033e2f32de9b4708d7
ZanborPanel <141851980+ZanborPanel@users.noreply.github.com>
2023-08-22 13:26:30 +0330

    Update zanbor.sh

--- zanbor.sh
+++ zanbor.sh
@@ -157,6 +157,7 @@ else
     dbpass=$dbpass
 fi
 
+sshpass -p $ROOT_PASSWORD mysql -u root -p -e "SET GLOBAL validate_password.policy = LOW;"
 sshpass -p $ROOT_PASSWORD mysql -u root -p -e "CREATE DATABASE $dbname;" -e "CREATE USER '$dbuser'@'%' IDENTIFIED WITH mysql_native_password BY '$dbpass';GRANT ALL PRIVILEGES ON * . * TO '$dbuser'@'%';FLUSH PRIVILEGES;" -e "CREATE USER '$dbuser'@'localhost' IDENTIFIED WITH mysql_native_password BY '$dbpass';GRANT ALL PRIVILEGES ON * . * TO '$dbuser'@'localhost';FLUSH PRIVILEGES;"
 
 colorized_echo green "[+] The robot database was created successfully!"
