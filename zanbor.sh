02296aaa1dff940f0324710b7d94588ed4e1fbe6
ZanborPanel <141851980+ZanborPanel@users.noreply.github.com>
2023-08-24 21:13:41 +0330

    Update zanbor.sh

--- zanbor.sh
+++ zanbor.sh
@@ -220,5 +220,5 @@ curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id="$
 
 sleep 2
 clear
-colorized_echo green "[+] The ZanborPanel Bot Has Been Successfully Installed\n"
+colorized_echo green "[+] The ZanborPanel Bot Has Been Successfully Installed"
 colorized_echo green "[+] Telegram channel: @ZanborPanel || Telegram group: @ZanborPanelGap"
