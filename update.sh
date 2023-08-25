57317f98f918a20210aeb26b4d500627b9824bb1
ZanborPanel <141851980+ZanborPanel@users.noreply.github.com>
2023-08-25 13:51:06 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -60,11 +60,11 @@ do
                 mv /var/www/html/zanbor.install /var/www/html/ZanborPanelBot/install/zanbor.install
                 sleep 2
 
-                token=$(grep -oP '(?<="token": ")[^"]*' /var/www/html/zanbor.install)
-                dev=$(grep -oP '(?<="dev": ")[^"]*' /var/www/html/zanbor.install)
-                db_name=$(grep -oP '(?<="db_name": ")[^"]*' /var/www/html/zanbor.install)
-                db_username=$(grep -oP '(?<="db_username": ")[^"]*' /var/www/html/zanbor.install)
-                db_password=$(grep -oP '(?<="db_password": ")[^"]*' /var/www/html/zanbor.install)
+                token=$(grep -oP '(?<="token": ")[^"]*' /var/www/html/ZanborPanelBot/install/zanbor.install)
+                dev=$(grep -oP '(?<="dev": ")[^"]*' /var/www/html/ZanborPanelBot/install/zanbor.install)
+                db_name=$(grep -oP '(?<="db_name": ")[^"]*' /var/www/html/ZanborPanelBot/install/zanbor.install)
+                db_username=$(grep -oP '(?<="db_username": ")[^"]*' /var/www/html/ZanborPanelBot/install/zanbor.install)
+                db_password=$(grep -oP '(?<="db_password": ")[^"]*' /var/www/html/ZanborPanelBot/install/zanbor.install)
                 echo -e "Your Bot Token: ${token}"
 
                 source_file="/var/www/html/ZanborPanelBot/config.php"
